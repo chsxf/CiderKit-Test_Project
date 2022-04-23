f30d3a92265086c2d1972353213c9c34dd623566
Christophe SAUVEUR <chsxf.pro@gmail.com>
2022-04-23 10:22:24 +0200

    🎉 Initial commit

---
+++ CiderKit-TestGame/CiderKit-TestGame/CiderKit_TestGameApp.swift
@@ -0,0 +1,59 @@
+import Cocoa
+import SwiftUI
+import CiderKit_Engine
+
+@main
+class CiderKit_TestGameApp: NSObject, NSApplicationDelegate {
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+    private func setup() -> Void {
+        let windowRect = NSRect(x: 100, y: 100, width: 640, height: 360)
+        let window = NSWindow(contentRect: windowRect, styleMask: [.titled, .closable, .resizable], backing: .buffered, defer: false)
+        window.acceptsMouseMovedEvents = true
+        window.title = "CiderKit Test Game"
+        let gameView = GameView(frame: windowRect)
+        window.contentView = gameView
+        window.makeKeyAndOrderFront(nil)
+        window.toggleFullScreen(nil)
+        
+        let url = Bundle.main.url(forResource: "map", withExtension: "ckmap")
+        gameView.loadMap(file: url!)
+    }
+    
+    private func setupMainMenu() -> Void {
+        let mainMenu = NSMenu()
+        
+        let menuItemOne = NSMenuItem()
+        menuItemOne.submenu = NSMenu(title: "menuItemOne")
+        menuItemOne.submenu?.items = [
+            NSMenuItem(title: "Quit CiderKit Test Game", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
+        ]
+        
+        mainMenu.items = [menuItemOne]
+        NSApp.mainMenu = mainMenu
+    }
+    
+    static func main() -> Void {
+        NSApp = NSApplication.shared
+        
+        let delegate = CiderKit_TestGameApp()
+        NSApp.delegate = delegate
+        
+        Task.detached {
+            try? await Atlases.preload(atlases: [
+                "main": "Main Atlas"
+            ])
+            
+            DispatchQueue.main.async {
+                delegate.setup()
+                delegate.setupMainMenu()
+            }
+        }
+        
+        NSApp.run()
+    }
+
+}
